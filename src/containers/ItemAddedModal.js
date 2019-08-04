4932fcf939631f3274953f1c3afb0f7e7c2977cf
BenLee3 <bendesigns76@gmail.com>
2019-08-04 14:26:19 +0900

    Add env variable to override babel loader error

--- src/containers/ItemAddedModal.js
+++ src/containers/ItemAddedModal.js
@@ -7,7 +7,7 @@ class ItemAddedModal extends React.Component {
   render () {
     return (
       <div>
-          <Modal className="item-added-modal" isOpen={this.props.open} toggle={this.props.toggle} className={this.props.className}>
+          <Modal className="item-added-modal" isOpen={this.props.open} toggle={this.props.toggle}>
             <ModalHeader className="item-added-modal-header">
               <p className="item-added-modal-p">Item Added To Cart</p>
             </ModalHeader>
